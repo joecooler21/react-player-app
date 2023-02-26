589e5efc0973c5760aa9dd8dd4e8157ef47ee5ae
Joe Cooler <joe_cooler@msn.com>
2023-02-25 20:50:08 -0600

    misc ui changes, bug fixes and close to getting loop synched with timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Player, PitchShift, Buffer, Transport } from 'tone'
 import { Button, Box, ButtonGroup, Slider, TextField, Select, InputLabel, MenuItem, FormControl } from '@mui/material'
 import './App.css'
@@ -24,12 +24,12 @@ function App() {
   const [sound, setSound] = useState(null)
   const [speed, setSpeed] = useState(1)
   const [playbackSpeed, setPlaybackSpeed] = useState('1.0');
-  const [loop, setLoop] = useState({ start: null, end: null })
+  const [loop, setLoop] = useState(false)
   const [duration, setDuration] = useState(0)
   const [rangeValue, setRangeValue] = useState([0, 0])
   const [singleValue, setSingleValue] = useState(0)
-  const [loopMode, setLoopMode] = useState('false')
   const [position, setPosition] = useState('00:00:00')
+  const [timer, setTimer] = useState(null)
 
   const defaultTextColor = '#3f50b5'
 
@@ -66,8 +66,6 @@ function App() {
 
     if (!sound) return
 
-    let timer = null
-
     if (!play) {
       const playback_rate = playbackSpeed
       // set playback rate
@@ -77,11 +75,14 @@ function App() {
       let pc = getpc(playback_rate)
       let pitch_shift = new PitchShift({ pitch: pc }).toDestination()
       sound.disconnect();
+      
       sound.connect(pitch_shift);
 
       sound.start()
+      sound.seek(singleValue, '+0')
       Transport.start()
-      timer = setInterval(() => {
+      setTimer(setInterval(() => {
+
         let seconds = Transport.seconds // get current playback position
 
         if (Math.round(seconds) === Math.round(duration)) { // is playback position at the end?
@@ -89,9 +90,9 @@ function App() {
           setPlay(false)
         }
 
-        setSingleValue(Math.round(seconds)) // move slider in accordance with current playback position
+        setSingleValue(Math.round(seconds))// move slider in accordance with current playback position
         setPosition(formatTime(seconds))
-      }, 1000)
+      }, 1000))
 
     }
 
@@ -107,12 +108,27 @@ function App() {
 
   const playLoop = () => {
     if (!sound) return
-    loopMode ? setLoopMode(false) : setLoopMode(true)
-    /* if (!sound) return
-    sound.loopStart = value[0]
-    sound.loopEnd = value[1]
-    sound.loop = true
-    sound.start()*/
+    if (!loop) {
+      clearInterval(timer)
+      sound.loop = true
+      sound.seek(rangeValue[0])
+      sound.start()
+      
+      setSingleValue(rangeValue[0])
+
+      setTimer(setInterval(() =>{
+        let seconds = Math.round(Transport.seconds)
+        console.log(seconds)
+        setSingleValue(seconds)
+        setPosition(formatTime(seconds))
+
+      }, 1000))
+
+    } else {
+      sound.loop = false
+
+    }
+    loop? setLoop(false) : setLoop(true)
   }
 
   const fileSelect = (e) => {
@@ -152,17 +168,19 @@ function App() {
   const setLoopRange = (e, newVal) => {
     if (!sound) return
     setRangeValue(e.target.value)
+    sound.loopStart = newVal[0]
+      sound.loopEnd = newVal[1]
 
   }
 
   const styles = {
     position: 'absolute',
-    left: '30%',
+    left: '22.5%',
     top: '30%',
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
-    width: '40%',
+    width: '55%',
     height: 'fit-content',
     padding: '0 0 2em 0',
     borderRadius: '10px',
@@ -174,17 +192,18 @@ function App() {
     <Box className='glass' sx={styles}>
 
       <div style={{ border: 'none', marginBottom: '1em', fontStyle: 'italic' }}>
-        <div style={{ padding: '.5em', fontSize: '1.1em', fontWeight: '100' }}>
+        <div className='text-shadow' style={{ padding: '.5em', fontSize: '1.1em', fontWeight: '100' }}>
           <Note sx={{ position: 'absolute', left: '0%', color: defaultTextColor }} />{fileName}</div>
 
         <hr style={{ opacity: '.5' }}></hr>
-        <div className='time-display'>{position}</div>
+        <div className='time-display text-shadow'>{position}</div>
 
       </div>
 
       <div>
         <Slider className='darken'
-         sx={{width: 300, color: 'success.primary','& .MuiSlider-thumb': { borderRadius: '1px', width: '.5em' },}}
+         sx={{width: 300, color: 'success.primary','& .MuiSlider-thumb': { borderRadius: '1px', width: '.5em' },
+         '& .MuiSlider-valueLabel': {backgroundColor:'transparent', color:'black', fontStyle:'italic'}}}
           value={singleValue}
           min={0}
           max={duration}
@@ -193,12 +212,14 @@ function App() {
           defaultValue={0} getAriaLabel={() => 'Default'}
           valueLabelDisplay='auto' valueLabelFormat={position} />
 
-        <Slider className='darken'
+        <Slider
+        className='darken'
         onChange={setLoopRange}
         value={rangeValue}
           sx={{ width: 300, color: 'success.primary', '& .MuiSlider-thumb': { borderRadius: '1px', width: '.5em' },
-           '& .MuiSlider-rail': {backgroundColor:'transparent'} }}
-          style={{ display: loopMode ? 'none' : 'inline-block', width: '90%', padding: '0em', marginBottom: '2em'}}
+           '& .MuiSlider-rail': {backgroundColor:'transparent'},
+            '& .MuiSlider-valueLabel': {backgroundColor:'transparent', color:'black', top:'3.5em', fontStyle:'italic'}}}
+          style={{ width: '90%', padding: '0em', marginBottom: '2em'}}
           min={0}
           defaultValue={rangeValue}
           max={duration}
@@ -213,10 +234,8 @@ function App() {
         <Button className='darken' component='label'><File /><input onChange={fileSelect} type='file' hidden /></Button>
         <Button className='darken'><Stop /></Button>
         <Button className='darken' onClick={playMode}>{play ? <Pause /> : <Play />}</Button>
-        <Button className='darken'>A</Button>
-        <Button className='darken'>B</Button>
         <Button className='darken'
-          style={{ filter: loopMode ? 'brightness(55%)' : 'brightness(100%)', color: loopMode ? defaultTextColor : 'white' }}
+          style={{ filter: loop ? 'brightness(100%)' : 'brightness(55%)', color: loop ? 'white':defaultTextColor}}
           onClick={playLoop}><Loop /></Button>
 
 
