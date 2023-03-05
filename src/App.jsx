8830174a8db71cecff83ba2730887c8110112f40
Joe Cooler <joe_cooler@msn.com>
2023-03-05 13:06:38 -0600

    updated ui with ipod theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
 import { useState, useEffect, useRef } from 'react'
 import { Player, PitchShift, Buffer, Transport } from 'tone'
-import { Button, Box, ButtonGroup, Slider, TextField, Select, InputLabel, MenuItem, FormControl } from '@mui/material'
+import { Button, Box, Slider } from '@mui/material'
 import './App.css'
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 
-import { default as Play } from '@mui/icons-material/PlayCircleFilledWhiteOutlined'
+import { default as Play } from '@mui/icons-material/PlayArrow'
 import { default as Pause } from '@mui/icons-material/PauseCircleOutlineOutlined'
-import { default as File } from '@mui/icons-material/AudioFileOutlined';
-import { default as Stop } from '@mui/icons-material/StopCircleOutlined';
-import { default as Loop } from '@mui/icons-material/Loop';
+import { default as File } from '@mui/icons-material/AudioFile'
+import { default as Stop } from '@mui/icons-material/Stop'
+import { default as Loop } from '@mui/icons-material/Loop'
+import { default as VolumeUp } from '@mui/icons-material/VolumeUp'
+import { default as VolumeDown } from '@mui/icons-material/VolumeDown'
 
 // valid playback rates and pitch correction values
-const pb_rates = { '1.0': 0, '0.95': 1, '0.90': 2, '0.85': 3, '0.80': 4, '0.75': 5,
-'0.70': 6, '0.65': 7.5, '0.60': 9, '0.55': 10.5, '0.50': 12 }
+const pb_rates = {
+  '1.0': 0, '0.95': 1, '0.90': 2, '0.85': 3, '0.80': 4, '0.75': 5,
+  '0.70': 6, '0.65': 7.5, '0.60': 9, '0.55': 10.5, '0.50': 12
+}
 
-const marks = [{value: 1.0,}, {value: 0.95},{value: 0.90,},{value: 0.85,},{value: 0.80,},{value: 0.75,},
-  {value: 0.70,}, {value: 0.65,},{value: 0.60,},{value: 0.55,},{value: 0.50,},];
+const marks = [{ value: 1.0, }, { value: 0.95 }, { value: 0.90, }, { value: 0.85, }, { value: 0.80, }, { value: 0.75, },
+{ value: 0.70, }, { value: 0.65, }, { value: 0.60, }, { value: 0.55, }, { value: 0.50, },];
 
 
 function App() {
@@ -77,7 +81,7 @@ function App() {
     if (!sound) return
 
     if (!play) {
-      
+
       const playback_rate = playbackSpeed
       // set playback rate
       sound.playbackRate = playback_rate
@@ -211,9 +215,9 @@ function App() {
 
   }, [loop])
 
-  useEffect (() => {
+  useEffect(() => {
     clearInterval(timer)
-    setInterval(()=>{
+    setInterval(() => {
       setSeconds(Math.round(Transport.seconds))
 
     }, Math.round(1000 / playbackSpeed))
@@ -240,45 +244,37 @@ function App() {
 
 
   return (
-    <Box className='glass center' sx={styles}>
+    <Box className='shell center' sx={styles}>
 
-      <div className='time-display'>
-        <p style={{fontSize:'.5em'}} className='display-text pixel'>{fileName}</p>
-        <p style={{ fontSize: '1.2em' }} className='display-text pixel'>{position}</p>
-        <p style={{fontSize:'.5em', position:'absolute', left:'32%', top:'24%'}}
-         className='display-text pixel'>speed: {playbackSpeed}</p>
+      <div className='time-display screen'>
+        <p style={{ fontSize: '.5em' }} className='display-text pixel'>{fileName}</p>
+        <p style={{ fontSize: '1.2em', position: 'absolute', left: '22.5%', top:'3%' }} className='display-text pixel'>{position}</p>
+        
 
-        {loop ? <div style={{position:'relative', bottom:'-15%',display:'flex', justifyContent:'space-around'}}>
-          <p style={{fontSize: '.5em'}} className='display-text pixel'>{formatTime(rangeValue[0])}</p>
-          <Loop style={{position:'absolute', top:'20%', fontSize:'.9em', color:'black'}} />
-          <p style={{fontSize:'.5em'}} className='display-text pixel'>{formatTime(rangeValue[1])}</p>
-        </div> : null}
-
-      </div>
-
-      <div>
-        <Slider className='darken'
+        <Slider
           sx={{
-            color: 'lightgrey', '& .MuiSlider-thumb': { borderRadius: '1px', width: '.5em' },
-            '& .MuiSlider-valueLabel': { backgroundColor: 'transparent', color: 'lightgrey', fontStyle: 'italic' }
+            color: 'lightgrey', height: '.1em', '& .MuiSlider-thumb': { borderRadius: '1px', width: '.25em', color: 'black', height: '.5em' },
+            '& .MuiSlider-valueLabel': { backgroundColor: 'transparent', color: 'lightgrey', fontStyle: 'italic' },
+            '& .MuiSlider-rail': { color: 'black' }, '& .MuiSlider-track': { color: 'black' }
           }}
           value={singleValue}
           min={0}
           max={duration}
           onChange={seek}
-          style={{ width: '70%', padding: '0em', marginBottom: '1em' }}
+          style={{ width: '70%', padding: '0em', position: 'absolute', left: '15%', top: '30%' }}
           defaultValue={0} getAriaLabel={() => 'Default'}
           valueLabelDisplay='off' valueLabelFormat={position} />
 
+          { /* loop controls */}
         <Slider
           onChange={setLoopRange}
           value={rangeValue}
           sx={{
-            visibility: loop ? 'visible' : 'hidden', color: 'lightgrey', '& .MuiSlider-thumb': { borderRadius: '1px', width: '.2em' },
-            '& .MuiSlider-rail': { backgroundColor: 'transparent' }, '& .MuiSlider-track': { color: 'lightgreen' },
+            visibility: loop ? 'visible' : 'hidden', color: 'lightgrey', '& .MuiSlider-thumb': { borderRadius: '1px', height: '.5em', width: '.2em', color: 'black' },
+            '& .MuiSlider-rail': { backgroundColor: 'transparent', width: '.2em' }, '& .MuiSlider-track': { color: 'black', width: '.2em' },
             '& .MuiSlider-valueLabel': { backgroundColor: 'transparent', color: 'lightgrey', top: '3.5em', fontStyle: 'italic' }
           }}
-          style={{ width: '70%', padding: '0em', marginBottom: '2em' }}
+          style={{ height: '.01em', width: '70%', padding: '0em', top: '15%' }}
           min={0}
           defaultValue={rangeValue}
           max={duration}
@@ -286,53 +282,69 @@ function App() {
           valueLabelDisplay='off'
           valueLabelFormat={(text, index) => { return formatTime(text) }}
         />
+        <Button
+          style={{zIndex:'1', color: loop ? 'black' : 'darkgrey', position: 'absolute', left: '34%', top: '38%', transform: 'scale(.7)' }}
+          onClick={playLoop}><Loop />
+        </Button>
+
+        {loop ? <div style={{ position: 'relative', top: '15%', display: 'flex', justifyContent: 'space-around' }}>
+          <p style={{ fontSize: '.5em' }} className='display-text pixel'>{formatTime(rangeValue[0])}</p>
+
+
+
+          <p style={{ fontSize: '.5em' }} className='display-text pixel'>{formatTime(rangeValue[1])}</p>
+        </div> : null}
+
+      </div>
+      <div>
       </div>
 
       <div>
-
-        <Button style={{ color: 'lightgrey' }} component='label'><File />
+          {/* play controls */}
+        <div style={{width:'100%', height:'30px', display:'flex', justifyContent:'center', position:'absolute', left:'0%', top:'55%'}}>
+          <Button sx={{width:'auto', height:'100%', color:'black'}} component='label'><File />
         <input onChange={fileSelect} type='file' hidden accept='.mp3, .wav, .ogg, .aac, .m4a' /></Button>
+        <Button style={{  width:'auto', height:'100%', color:'black' }} onClick={playMode}><Play /></Button>
+        <Button  onClick={stop} style={{ width:'auto', height:'100%', color:'black'}} ><Stop /></Button>
+        </div>
 
-        <Button sx={{ color: 'lightgrey', transform: 'scale(1.5)' }} onClick={playMode}>{play ? <Pause /> : <Play />}</Button>
-        <Button onClick={stop} style={{ color: 'lightgrey' }} ><Stop /></Button>
-
-        <Button
-          sx={{ color: loop ? 'lightgreen' : 'lightgrey', position: 'absolute', left: '2em', bottom: '3em' }}
-          onClick={playLoop}><Loop /></Button>
-
-<Slider className='darken'
-          sx={{
-            color: 'lightgrey', '& .MuiSlider-thumb': { borderRadius: '1px', width: '.5em' },
+        {/* volume controls */}
+        <div style={{width:'175px', height:'30px', position:'absolute', right:'15%', bottom:'20%', display:'flex',
+         flexDirection:'row', justifyContent:'space-around'}}>
+          <VolumeDown sx={{transform:'scale(.7)'}} />
+        <Slider
+          sx={{borderRadius:'0px', width:'100px', height:'1px', position:'relative', bottom:'5%',
+            color: 'black', '& .MuiSlider-thumb': { borderRadius: '1px', width:'5px', height:'10px' },
             '& .MuiSlider-valueLabel': { backgroundColor: 'transparent', color: 'lightgrey', fontStyle: 'italic' }
           }}
           value={volume}
           min={0}
           max={100}
           onChange={volumeSlider}
-          style={{ width: '100px', position:'absolute', right:'20%', bottom:'15%'}}
           defaultValue={100} getAriaLabel={() => 'Default'}
-          valueLabelDisplay='off' valueLabelFormat={position} />
-
-
-
+          valueLabelDisplay='off' valueLabelFormat={position}/>
+          <VolumeUp sx={{transform:'scale(.7)'}} />
+          </div>
+          
+          { /* speed controls */}
+          <div style={{width:'150px', display:'flex', justifyContent:'center', position:'absolute', bottom:'10%', left:'20%'}}>
+          <p style={{ fontSize: '.5em', marginRight:'5px'}}
+          className='display-text pixel'>Speed: {playbackSpeed}</p>
         <Slider
-          sx={{
-            '& .MuiSlider-thumb': { borderRadius: '1px', width: '.5em' },
-            width: '100px', color: 'lightgrey', position: 'absolute', right: '20%', bottom: '5%',
-            '& .MuiSlider-valueLabel': {
-              backgroundColor: 'transparent', color: 'lightgrey',
-               top:'1%',fontStyle: 'italic'
-            }
+          sx={{borderRadius:'0px', '& .MuiSlider-mark':{backgroundColor:'transparent'},
+            '& .MuiSlider-thumb': { borderRadius: '1px', width: '5px', height:'10px', color:'black' },
+            width: '50px', height:'1px', color:'black'
           }}
           aria-label="Custom marks"
           defaultValue={1.0}
           step={0.05}
           max={1.0}
           min={.5}
-          valueLabelDisplay="auto"
+          valueLabelDisplay="off"
           marks={marks}
           onChange={changePlaybackSpeed}
         />
+        </div>
       </div>
     </Box>
   )
